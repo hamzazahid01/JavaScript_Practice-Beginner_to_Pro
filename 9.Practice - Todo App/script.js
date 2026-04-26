67a8c0a2dd232647c9cdab7ffb502bc3ee188838
Hamza Zahid <hamzazahid0066example.com>
2026-04-26 09:11:31 +0500

    Todo App - Integrated LocalStorage with add, delete and complete features

--- 9.Practice - Todo App/script.js
+++ 9.Practice - Todo App/script.js
@@ -1,12 +1,25 @@
 
 let addBtn = document.querySelector("#addBtn");
-taskInput = document.querySelector("#taskInput");
+let taskInput = document.querySelector("#taskInput");
 let taskList = document.querySelector("#taskList");
+
+function saveTasks(){
+    let tasks = [];
+    let items = document.querySelectorAll("#taskList li span");
+    items.forEach(
+        function(item){
+            tasks.push(item.innerText);
+        }
+    );
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 addBtn.addEventListener("click", function(){
     let task = taskInput.value;
 
     if(taskInput.value===""){
         alert("Please enter a task!");
+        return;
     }
 
     if(task){
@@ -24,7 +37,7 @@ addBtn.addEventListener("click", function(){
         })
 
         span.addEventListener("click", function(){
-            li.classList.toggle("Completed")
+            li.classList.toggle("completed")
         })
 
         li.appendChild(span);
@@ -33,8 +46,41 @@ addBtn.addEventListener("click", function(){
         taskList.appendChild(li);
 
         taskInput.value="";
-
+        saveTasks();
     }
 })
+
+window.addEventListener("load",function(){
+    let savedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if (savedTasks){
+        savedTasks.forEach(function(task){
+
+            let li = document.createElement("li");
+
+            let span = document.createElement("span");
+            span.innerText=task;
+
+            let delBtn=document.createElement("button");
+            delBtn.innerText="Delete";
+
+            delBtn.addEventListener("click",function(){
+                li.remove();
+                saveTasks();
+            })
+            span.addEventListener("click", function(){
+                li.classList.toggle("completed");
+                saveTasks();
+            })
+
+            li.appendChild(span);
+            li.appendChild(delBtn);
+
+            taskList.appendChild(li);
+
+        })
+    }
+})
+
 
 
