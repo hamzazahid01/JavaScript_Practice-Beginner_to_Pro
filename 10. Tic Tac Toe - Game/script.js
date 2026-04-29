0a8c83ee9e7a8bd01392351bd5eb70a49af05f04
Hamza Zahid <hamzazahid0066example.com>
2026-04-29 09:13:55 +0500

    Tic Tac Toe - Fix: prevent overwriting cells and improve X/O toggle logic

---
+++ 10. Tic Tac Toe - Game/script.js
@@ -0,0 +1,61 @@
+
+let resetBtn = document.querySelector("#resetBtn");
+let status = document.querySelector("#status");
+
+let cells = document.querySelectorAll(".cell");
+
+const winPattern = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+function checkWin(){
+
+    winPattern.forEach((pattern)=>{
+
+        let pos1 = cells[pattern[0]].innerText;
+        let pos2 = cells[pattern[1]].innerText;
+        let pos3 = cells[pattern[2]].innerText;
+        
+        if(pos1!=="" && pos1==pos2 && pos2==pos3){
+            console.log("Winner Found!");
+            return true;
+        }
+
+    })
+}
+
+let player = true;
+
+cells.forEach((cell)=>{
+    cell.addEventListener("click", function(){
+
+        if(cell.innerText!=""){
+            return;
+        }
+
+        if(player){
+             cell.innerText = "0";
+             status.innerText = "Player X Turn";
+             player=false;
+             console.log("Player 1 clicked");
+        }
+        else{
+            cell.innerText = "X";
+            status.innerText = "Player 0 Turn";
+            player=true;
+            console.log("player 2 clicked")
+        }
+
+        checkWin();
+
+    })
+})
+
+
