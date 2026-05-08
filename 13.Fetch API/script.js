057ab0f514e87be797a04b134f4adc725cf280a2
Hamza Zahid <hamzazahid0066@gmail.com>
2026-05-08 13:55:29 +0500

    API Done with mini project

--- 13.Fetch API/script.js
+++ 13.Fetch API/script.js
@@ -1,9 +1,8 @@
 let url = "https://official-joke-api.appspot.com/random_joke";
 
 let setupLine = document.querySelector("#setup");
-
-let sLine;
-let pLine;
+let punchLine = document.querySelector("#punchline");
+let btn = document.querySelector("#btn");
 
 window.addEventListener("load",()=>{
     async function getJoke(){
@@ -12,9 +11,15 @@ window.addEventListener("load",()=>{
             let data = await response.json();
             console.log(data);
             sLine=data.setup;
-            pline=data.punchLine;
+            pLine=data.punchline;
 
-            setupLine.innerText = sLine;
+            setupLine.innerText = data.setup;
+            punchLine.innerText = "";
+
+            btn.addEventListener("click",()=>{
+              punchLine.innerText = data.punchline;
+            })
+  
 
         } catch(error){
             console.log("Error Alert");
